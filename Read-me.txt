ac34a1b7b2a6fb047311af4a0e3f35911c32b4c7
Benjamin Serra <benjaserrau@gmail.com>
2021-12-09 21:38:11 -0300

    first init

---
+++ Read-me.txt
@@ -0,0 +1,92 @@
+# Título del Proyecto
+
+_Demolicion de minerales_
+
+## Comenzando 🚀
+
+_Estas instrucciones te permitirán obtener una copia del proyecto en funcionamiento en tu máquina local para propósitos de desarrollo y pruebas._
+
+
+### Pre-requisitos 📋
+
+_Instalar el programa python 3.0 en caso de no tenerlo en nuestro computador_
+
+_Tener powershell como administrador. En caso de no tenerlo, ejecutamos como administrador y escribiremos lo siguente en la terminal de powershell: " Set-ExecutionPolicy Unrestricted "_ 
+
+
+### Instalación 🔧
+
+_Instalar requirements.txt de la siguente forma:_
+
+1. _Abrimos Visual Studio_
+
+2. _Escribimos en la terminal " python -m venv env " para crear un entorno virtual_
+
+3. _Ecribiendo en la terminal " cd env/Scripts "_
+
+4. _Dentro de la carpeta " Scripts " activamos el entorno virtual escribiendo en la terminal " activate "_ 
+
+5. _Luego de activar el entorno virtual, escribimos cd.. presionamos enter y volvemos a ecribir cd.._
+
+5. Finalmente instalamos los requerimientos escribiendo: "pip install -r requirements.txt"_
+
+_En caso de que aún aparezaca ciertas librerias sin instalar, escribimos en la terminal, las librerias particulates que no se instalaran, de la siguente manera: "pip install (nombre de la libreria)"_
+
+_De forma opcional, instalar por separado cada uno de los siguentes requeimientos:_
+
+1. backcall==0.2.0
+2. colorama==0.4.4
+3. cycler==0.11.0
+4. decorator==5.1.0
+5. fonttools==4.28.3
+6. ipython==7.30.1
+7. jedi==0.18.1
+8. kiwisolver==1.3.2
+9. matplotlib==3.5.0
+10. matplotlib-inline==0.1.3
+11. networkx==2.6.3
+12. numpy==1.21.4
+13. packaging==21.3
+14. pandas==1.3.4
+15. parso==0.8.3
+16. pickleshare==0.7.5
+17. Pillow==8.4.0
+18. prompt-toolkit==3.0.23
+19. Pygments==2.10.0
+20. pyparsing==3.0.6
+21. python-dateutil==2.8.2
+22. pytz==2021.3
+23. setuptools-scm==6.3.2
+24. six==1.16.0
+25. tomli==1.2.2
+26. traitlets==5.1.1
+27. wcwidth==0.2.5
+
+
+## Ejecutando las pruebas ⚙️
+
+_Una vez tenga todos los pasos anteriores realizados de manera correcta, se le solicitara abrir el archivos con el programa de preferencia, visual estudio_
+
+### Ejecucion programa 🔩
+
+_Se le pedira ingresar una cantidad de nodos y aristas. Luego de ingresar lo pedido, se abrirá una ventana con el grafo generado, mientras que en la terminal aparecerá el calculo de la matriz resultante, La excentricidad de todos los vértices y el diámetro de la red. Al cerrar la ventana del grafo generado, de inmediato se abrira un siguente grafo, con la propuesta de estrategia fija_
+
+### Estrategia seguida para reducir el diámetro y el costo de transporte ⌨️
+
+_Se añadieron nuevos bordes claves entre los nodos de mayor distancia, para asi asegurar la distancia entre estos, los cuales se reducen a 1 (distancia mínima entre 2 nodos fuertemente conectados para gráfico no ponderado), por lo que el diámetro se reduce así como el total distancia (costo)._
+
+## Construido con 🛠️
+
+_Menciona las herramientas que utilizaste para crear tu proyecto_
+
+* [Visual studio code](https://code.visualstudio.com)
+
+## integrantes 🖇️
+_benjamín Serra -_
+_sofía López -_
+_Benjamín zárate_
+
+
+## Expresiones de Gratitud 🎁
+
+ 📢🍺☕🤓❤️😊
---
+++ app.py
@@ -0,0 +1,105 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+import numpy as np
+from numpy import random
+import pandas as pd
+from IPython.display import display
+
+def draw_graph(n,m):
+    
+    # definir atributos de gráfico  
+    maximum_edges= n*(n-1)
+    minimum_edges= n
+    
+    # Inicializando nuestro gráfico dirigido
+    G=nx.DiGraph()
+    
+    #Agregando nodos uno tras otro de acuerdo con la entrada del usuario   
+    for i in range(1,n+1):
+        G.add_node(i)
+        
+        
+    # barajando nuestros nodos para que generemos los bordes aleatorios mínimos entre ellos para obtener un gráfico conectado
+    random_nodes= list(random.choice(list(G.nodes),size=G.number_of_nodes(),replace=False))          
+    for i in range(len(random_nodes)):    
+        if random_nodes[i] != random_nodes[-1]:
+            G.add_edge(random_nodes[i],random_nodes[i+1])
+        else:
+            G.add_edge(random_nodes[i],random_nodes[0])
+       
+    
+    # agregando el resto de bordes aleatoriamente hasta que alcancemos el número requerido de bordes por el usuario
+    while m!=G.number_of_edges() :
+            u,v=random.choice(list(G.nodes),size=2,replace=False)
+            G.add_edge(u,v)
+    
+            
+    # matriz de distancia de impresión
+    matrix= np.zeros(shape=(n,n))
+    for i in range(G.number_of_nodes()):
+        for j in range(G.number_of_nodes()):
+            matrix[i,j]=int(nx.shortest_path_length(G,i+1,j+1))
+            df=pd.DataFrame(matrix,index=range(1,G.number_of_nodes()+1),columns=range(1,G.number_of_nodes()+1))
+    print('Distance matrix equals:')
+    display(df)
+
+    
+    # número de impresión de bordes
+    print('Number of edges equals: ',G.number_of_edges(),'\n')
+    
+    # excentricidad de impresión      
+    for k,v in nx.eccentricity(G).items():
+        print(f'Eccentricity of node {k} equals: {v} \n')
+        
+    # diámetro de impresión
+    print('Diameter of the net equals: ',nx.diameter(G),'\n')
+    
+    # suma de todas las distancias (costo de transporte)
+    print('Transportation cost equals: ',np.sum(matrix),'\n')
+
+    
+    # Mostrando el gráfico
+    plt.figure(figsize=(8,6))
+    nx.draw(G,node_size=1000,with_labels=True)
+    plt.show()
+    
+    print(100*'-','\n')
+    print('Results after applying our strategy to reduce diameter and distance:\n')
+    
+    # disminución de la distancia total (agregamos nuevos bordes entre los nodos de mayor excentricidad)
+    for i in list(zip(np.where(matrix==nx.diameter(G))[0],np.where(matrix == nx.diameter(G))[1])):
+        G.add_edge(i[0]+1,i[1]+1)
+        matrix[i]=1
+        
+        
+    # número de impresión de bordes
+    print('Number of edges equals: ',G.number_of_edges(),'\n')
+        
+    # excentricidad de impresión
+    for k,v in nx.eccentricity(G).items():
+        print(f'Eccentricity of node {k} equals: {v} \n')
+        
+    # diámetro de impresión
+    print('Diameter of the net equals: ',nx.diameter(G),'\n')  
+    
+    
+    # Impresión del costo de transporte después de aplicar nuestra estrategia.
+    print('Transportation cost equals: ',np.sum(matrix),'\n')
+        
+    # Mostrando el gráfico
+    plt.figure(figsize=(8,6))
+    nx.draw(G,node_size=1000,with_labels=True)
+    plt.show()
+
+
+nodes = input("Ingrese el número de nodos como un número entero: >>> ")
+while not nodes.isnumeric():
+    nodes=input("Ingrese el número de nodos como un número entero: >>> ")
+nodes=int(nodes)
+
+edges=input(f"Introduzca el número de aristas como un número entero entre {nodes} y {nodes*(nodes-1)}:>>> ")
+while not edges.isnumeric() or not int(edges) in range(nodes,(nodes*(nodes-1))+1):
+    edges=input(f"Introduzca el número de aristas como un número entero entre {nodes} and {nodes*(nodes-1)}:>>> ")
+
+edges=int(edges)
+draw_graph(nodes,edges)
